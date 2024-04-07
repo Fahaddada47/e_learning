ffbb026c572b286ad19815dd7ffd7d90873ecd0b
fahaddada47 <fifahad007@gmail.com>
2024-04-08 01:51:41 +0600

    update

--- lib/app/state_holder_binder.dart
+++ lib/app/state_holder_binder.dart
@@ -1,9 +1,9 @@
-// import 'package:e_learning/features/domain/state_holders/course_description_controller.dart';
-// import 'package:get/get.dart';
-//
-// class StateHolderBinder extends Bindings {
-//   @override
-//   void dependencies() {
-//     Get.put(CourseDetailsController());
-//   }
-// }
+import 'package:e_learning/features/domain/state_holders/course_description_controller.dart';
+import 'package:get/get.dart';
+
+class StateHolderBinder extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(HomeController());
+  }
+}
