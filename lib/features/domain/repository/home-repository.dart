d541af874dc0cbdd05c9006796c5d49a10ff6f4d
fahaddada47 <fifahad007@gmail.com>
2024-04-08 01:55:35 +0600

    update

--- lib/features/domain/repository/home-repository.dart
+++ lib/features/domain/repository/home-repository.dart
@@ -4,6 +4,7 @@ import 'dart:ui';
 
 import 'package:e_learning/features/data/model/course_model.dart';
 import 'package:e_learning/features/data/network/network_api_services.dart';
+import 'package:e_learning/features/data/utility/urls.dart';
 
 
 class HomeRepository {
@@ -11,7 +12,7 @@ class HomeRepository {
   final _apiService  = NetworkApiServices() ;
 
   Future<CourseModel> courseListApi() async{
-    dynamic response = await _apiService.getApi("https://getlearn-admin.getbuildfirst.com/api/course/details/1");
+    dynamic response = await _apiService.getApi(Urls.getCourse);
     return CourseModel.fromJson(response) ;
   }
 
